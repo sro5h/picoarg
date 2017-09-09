07d26c30f31c28bf0bbdb42f37de3b6099f2317b
Paul Meffle <meffle.paul@me.com>
2017-09-09 16:39:51 +0200

    Add info comment

--- picoarg.hpp
+++ picoarg.hpp
@@ -1,3 +1,18 @@
+/*
+   picoarg.hpp - 1.0.1
+
+   Author:
+        Paul Meffle
+
+   Summary:
+        picoarg is a single-file header that implements a simple commandline
+        option parser.
+
+   Revision history:
+        1.0   (15.08.2017) initial release
+        1.0.1 (09.09.2017) allow arguments multiple times
+*/
+
 #ifndef _PICO_ARG_HPP
 #define _PICO_ARG_HPP
 
