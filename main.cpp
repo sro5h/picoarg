bc3c2806312969ffecfdbe3cbb0742a49cf20791
Paul Meffle <meffle.paul@me.com>
2017-09-14 13:00:23 +0200

    Add 'h' option to example

--- main.cpp
+++ main.cpp
@@ -6,11 +6,19 @@
 int main(int argc, char* argv[])
 {
         OptionParser parser;
+        parser.add('h');
         parser.add('v');
         parser.add('f', true);
 
         if (!parser.parse(argc, argv)) {
                 return -1;
+        }
+
+        if (parser.has('h')) {
+                std::cout << "Usage: a.out [OPTION]" << std::endl;
+                std::cout << "  -v        show version information" << std::endl;
+                std::cout << "  -f<file>  process <file>" << std::endl;
+                return 0;
         }
 
         if (parser.has('v')) {
